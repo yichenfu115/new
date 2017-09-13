14ea2aee225ae329dfd968e3646726bfc2212dd7
yichenfu115 <icfu_0309@yahoo.com.tw>
2017-09-13 16:13:26 +0800

    first commit

---
+++ inc/main.h
@@ -0,0 +1,6 @@
+
+
+#ifndef __MAIN_H
+#define __MAIN_H
+#include "stm32f0xx.h"
+#endif /* __MAIN_H */
---
+++ inc/stm32f0xx_it.h
@@ -0,0 +1,20 @@
+
+#ifndef __STM32F0XX_IT_H
+#define __STM32F0XX_IT_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif 
+#include "main.h"
+void NMI_Handler(void);
+void HardFault_Handler(void);
+void SVC_Handler(void);
+void PendSV_Handler(void);
+void SysTick_Handler(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif 
+
